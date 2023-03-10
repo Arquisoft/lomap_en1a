6de4f672bb704c8fc4ae78de4b170909213924be
Sergio del Rey <UO282497@uniovi.es>
2023-03-10 23:24:24 +0100

    Fix

--- restapi/repositories/UserRepositoryImpl.ts
+++ restapi/repositories/UserRepositoryImpl.ts
@@ -1,11 +1,40 @@
+import { resolve } from "path";
 import { UserRepository } from "../business/repositories/UserRepository";
 import { User } from "../domain/User";
+import { MySql } from "../src/persistence/db/MySql";
 
 export class UserRepositoryImpl implements UserRepository {
 
     private u: User = new User("1", "User 1", "podId");
+    private mysql: MySql = MySql.getInstance();
 
-    findById(id: string): User {
-        return this.u;
+    findById(id: string): Promise<User> {
+        return new Promise((resolve, reject) => {
+            this.mysql.con.query(
+                "SELECT * FROM USERS WHERE PODID = '" + id + "';",
+                (err: any, result: any, fields: any) => {
+                    var length = Object.keys(result).length;
+                    var places: Place[] = new Array(length);
+                    Object.keys(result).forEach(function (key) {
+                        var aux: number = +key;
+                        var row = result[key];
+                        places[aux] = new Place(
+                            row.PLACE_ID,
+                            row.NAME,
+                            row.OWNER_ID,
+                            (<any>PlaceVisibility)[row.VISIBILITY],
+                            row.LATITUDE,
+                            row.LONGITUDE
+                        );
+
+
+
+
+                    });
+
+                    resolve(places);
+                }
+            );
+        });
     }
 }
