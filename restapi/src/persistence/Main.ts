f4ce69a1d300936b70f737ce13cf51394f7c6990
UO282476 <UO282476@uniovi.es>
2023-03-11 10:57:39 +0100

    Simple test

--- restapi/src/persistence/Main.ts
+++ restapi/src/persistence/Main.ts
@@ -2,12 +2,18 @@ import { CommentServiceImpl } from "../../business/comment/CommentServiceImpl";
 import { PlaceServiceImpl } from "../../business/place/PlaceServiceImpl";
 import { UserDAO } from "./user/UserDAO";
 import { Comment } from "../../domain/Comment";
+import { PlaceDto } from "../../domain/dtos/PlaceDto";
 
 const a = new UserDAO();
 
 
 var c: CommentServiceImpl = new CommentServiceImpl();
 
-var comment: Promise<Comment> = c.findById("1");
+var place: PlaceDto = new PlaceDto();
+place.id = "1";
 
-console.log(comment);
+var comment: Promise<Comment[]> = c.findByPlace(place);
+
+comment.then(c => {
+    console.log(c);
+})
