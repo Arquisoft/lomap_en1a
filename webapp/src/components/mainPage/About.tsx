9250cceed5e4b30f3c42fb6939b99e746ca91e50
Diego-274 <diegoreyber@hotmail.com>
2023-04-03 15:06:31 +0200

    About page added

--- webapp/src/components/mainPage/About.tsx
+++ webapp/src/components/mainPage/About.tsx
@@ -1,5 +1,21 @@
 
 
 export default function About() {
-  return <p>PLACEHOLDER</p>
+  return (
+    <>
+      <div className="centered-block text-area">
+        <h1>Who are we?</h1>
+        <p>
+          We are a group of students from the University of Oviedo (UniOvi), from the Software Engineering degree.
+          This app has been developed for the Subject Architecture subject, whose goal is to help us practice working
+          on big projects in teams composed by many people.
+          <br/>
+          <br/>
+          Our team is made up from six programming beginners who are passionated about learning everything they can 
+          about all the things related to software and computering, and this app is the result of all the hard work 
+          and time we have spent over two months.
+        </p>
+      </div>
+    </>
+  )
 }
