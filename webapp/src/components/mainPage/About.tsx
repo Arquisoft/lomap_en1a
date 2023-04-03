aa3955ceac35a36b43a494931ab89fdd6c7ecb0e
Diego-274 <diegoreyber@hotmail.com>
2023-04-03 16:32:38 +0200

    Fixed grammar error

--- webapp/src/components/mainPage/About.tsx
+++ webapp/src/components/mainPage/About.tsx
@@ -7,12 +7,12 @@ export default function About() {
         <h1>Who are we?</h1>
         <p>
           We are a group of students from the University of Oviedo (UniOvi), from the Software Engineering degree.
-          This app has been developed for the Subject Architecture subject, whose goal is to help us practice working
+          This app has been developed for the Software Architecture subject, whose goal is to help us practice working
           on big projects in teams composed by many people.
           <br/>
           <br/>
-          Our team is made up from six programming beginners who are passionated about learning everything they can 
-          about all the things related to software and computering, and this app is the result of all the hard work 
+          Our team is made up from six programming beginners who are passionate about learning everything they can 
+          about all the things related to software and computing, and this app is the result of all the hard work 
           and time we have spent over two months.
         </p>
       </div>
