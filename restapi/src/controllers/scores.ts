8d44cdc8f2c531f294e5258c540da74f4b164a53
UO281801 <UO281801@uniovi.es>
2023-04-07 18:13:14 +0200

    Scores fixed

--- restapi/src/controllers/scores.ts
+++ restapi/src/controllers/scores.ts
@@ -39,18 +39,20 @@ module.exports = function (api: Router, service: ScoreService) {
                 Assertion.exists(req.body.user, "A user must be provided.");
                 Assertion.exists(req.body.score, "An score must be provided.");
                 Assertion.exists(req.body.place, "A place must be provided.");
-                Assertion.exists(req.body.visibility, "A visibility must be provided.");
+                //Assertion.exists(req.body.visibility, "A visibility must be provided.");
                 Assertion.exists(req.session.solidSessionId, "The user must be logged in.");
 
                 var sessionId: string = <string>req.session.solidSessionId;
                 var punt: number = <number>req.body.score;
                 var placeId: string = <string>req.body.place;
-                var visibility: Visibility = <Visibility>req.params.visibility;
+                //var visibility: Visibility = <Visibility>req.params.visibility;
 
                 var score: ScoreDto = new ScoreDto();
                 score.score = punt;
                 score.place = placeId;
-                score.visibility = visibility;
+                //score.visibility = visibility;
+                //FIXME
+                score.visibility = Visibility.PUBLIC;
 
                 return res.send(await service.add(sessionId, score));
             }
