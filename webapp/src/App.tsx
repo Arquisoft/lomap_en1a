5a1d4c6495bdf51bb4793da0f2d0734409bae9c6
Álvaro González Erigoyen <uo282790@uniovi.es>
2023-04-10 01:49:26 +0200

    Revising documentation of Section 5

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,10 +1,10 @@
 import NavBar from "./components/mainPage/NavBar"
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import Home from "./components/mainPage/Home"
 import About from "./components/mainPage/About"
 import Contact from "./components/mainPage/Contact"
 import Footer from "./components/mainPage/Footer"
-import { Route, Routes} from "react-router-dom"
+import {Route, Routes} from "react-router-dom"
 import "./App.css";
 import LoginForm from "./components/mainPage/LoginForm"
 import MapView from "./components/map/MapView"
@@ -12,31 +12,30 @@ import UserProfile from "./components/mainPage/UserProfile"
 import "react-sliding-pane/dist/react-sliding-pane.css";
 
 
-
 function App() {
 
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+    // const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  return (
-    
-    <div className="background-image container">
-      <NavBar /*isLoggedIn={isLoggedIn}*//>
-          <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/login" element={<LoginForm /*setIsLoggedIn={setIsLoggedIn}*//>} />
-                <Route path="/map" element={<MapView/>} />
-                <Route path="/profile" element={<UserProfile/>} />
-              </Routes>
+    return (
 
-        
-      <Footer/>
-    
-    </div>
+        <div className="background-image container">
+            <NavBar /*isLoggedIn={isLoggedIn}*//>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+                <Route path="/login" element={<LoginForm /*setIsLoggedIn={setIsLoggedIn}*//>}/>
+                <Route path="/map" element={<MapView/>}/>
+                <Route path="/profile" element={<UserProfile/>}/>
+            </Routes>
 
-    
-  )
+
+            <Footer/>
+
+        </div>
+
+
+    )
 }
 
 export default App
