898925d21fba0249730dae7a2bdb114b9a121e4a
PabVF23 <60737771+PabVF23@users.noreply.github.com>
2023-04-26 21:07:33 +0200

    Removed unnecessary text

--- webapp/src/components/mainPage/Contact.tsx
+++ webapp/src/components/mainPage/Contact.tsx
@@ -11,7 +11,7 @@ export default function Contact(){
               <br/>
               These are our contact emails:
               <ul className="email">
-                <li>Vanesa Alonso Ramos: <a href="mailto:UO281801@uniovi.es" color="blue">UO281801@uniovi.es</a></li>
+                <li>Vanesa Alonso Ramos: <a href="mailto:UO281801@uniovi.es">UO281801@uniovi.es</a></li>
                 <li>Álvaro González Erigoyen: <a href="mailto:UO282790@uniovi.es">UO282790@uniovi.es</a></li>
                 <li>Diego Moragón Merallo: <a href="mailto:UO284016@uniovi.es">UO284016@uniovi.es</a></li>
                 <li>Rubén del Rey Álvarez: <a href="mailto:UO282476@uniovi.es">UO282476@uniovi.es</a></li>
