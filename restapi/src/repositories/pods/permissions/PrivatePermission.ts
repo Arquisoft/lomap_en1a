b28abf17d5e7cde1c5d1ce509fbb7395e69c1fc4
UO282476 <UO282476@uniovi.es>
2023-04-30 22:01:53 +0200

    Added error hadling to the permission management

--- restapi/src/repositories/pods/permissions/PrivatePermission.ts
+++ restapi/src/repositories/pods/permissions/PrivatePermission.ts
@@ -5,19 +5,21 @@ import { access } from "@inrupt/solid-client";
 
 export class PrivatePermission implements Permission {
   async setAcl(sessionId: string, url: string): Promise<void> {
-    let session = await getSessionFromStorage(sessionId);
+    try {
+      let session = await getSessionFromStorage(sessionId);
 
-    let webId = await PodManager.sessionManager.getCurrentWebId(sessionId);
+      let webId = await PodManager.sessionManager.getCurrentWebId(sessionId);
 
-    if (session == null) {
-      throw new Error("The user must be logged in.");
-    }
+      if (session == null) {
+        throw new Error("The user must be logged in.");
+      }
 
-    await access.setAgentAccess(
-      url,
-      webId.split("/profile")[0] + "/profile/card#me",
-      { read: true },
-      { fetch: session.fetch }
-    );
+      await access.setAgentAccess(
+        url,
+        webId.split("/profile")[0] + "/profile/card#me",
+        { read: true },
+        { fetch: session.fetch }
+      );
+    } catch (e) {}
   }
 }
