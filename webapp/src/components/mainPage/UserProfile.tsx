b962f43279c1f5dc7469131c339b924c66ad62f0
Diego-274 <diegoreyber@hotmail.com>
2023-04-30 15:16:22 +0200

    Profile view improved

--- webapp/src/components/mainPage/UserProfile.tsx
+++ webapp/src/components/mainPage/UserProfile.tsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import { User } from "../../domain/User";
 import placeholderImage from '../../images/user_icon.png';
 import { getProfile } from "../../api/api";
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import { Divider } from '@material-ui/core';
 
 
 export default function UserProfile(): JSX.Element {
 
-    //Get the list of places for the current user
     const [profile, setProfile] = useState<User>();
     const refreshProfile = async () => {
         getProfile().then((user) => setProfile(user));
@@ -19,21 +21,39 @@ export default function UserProfile(): JSX.Element {
 
     return (
       <>
-        <div className="centered-block text-area profile-view">
-          <div id="profile-pic-div"><img id="profile-pic" src={placeholderImage}></img></div>
-          <ProfileAttribute label="Username" text={profile?.username}></ProfileAttribute>
-          <ProfileAttribute label="WebID" text={profile?.webId}></ProfileAttribute>
-        </div>
+        <Grid container className="centered-grid grid-text-area" spacing={3} justifyContent="space-around" 
+            width={'80%'} height={'80%'} marginLeft={'auto'} marginRight={'auto'} padding={"1.5em 0em"}>
+          <Grid item xs={12} textAlign={"center"}>
+            <Box component="img" 
+              src={profile?.photo ? profile?.photo : placeholderImage} 
+              sx={{ 
+                  objectFit: "cover", width: '12em', height: '12em', borderRadius: '50%' }}>
+            </Box>
+          </Grid>
+          <Divider/>
+          <Grid item xs={12}>
+            <ProfileAttribute label="Username" text={profile?.username}></ProfileAttribute>
+          </Grid>
+          <Grid item xs={12}>
+            <ProfileAttribute label="WebID" text={profile?.webId}></ProfileAttribute>
+          </Grid>
+          
+        </Grid>
       </>
     )
 }
 
 function ProfileAttribute(props: { label: string, text?: string }) {
     return (
-        <div className="profile-attribute">
-            <label>{props.label}</label><br/>
+      <Box className="profile-attribute"
+          height="100%"
+          display="flex"
+          justifyContent="center"
+          flexDirection="column">
+            <label>{props.label}</label>
             <div>{props.text}</div>
-        </div>
+      </Box>
+        
     )
 }
   
