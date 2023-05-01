e2c18b6bd2760379099be430daa0d173982a2d8e
PabVF23 <60737771+PabVF23@users.noreply.github.com>
2023-05-02 01:47:03 +0200

    Solved error in test

--- webapp/src/test/map/FriendPanel.test.tsx
+++ webapp/src/test/map/FriendPanel.test.tsx
@@ -10,13 +10,14 @@ jest.mock('../../api/api');
 
 test('check comment is added', async () => {
     let places = [
-        new Place("", "Place1", "Description", "Alice", 756, 123, Visibility.FRIENDS, Category.MONUMENT),
-        new Place("", "Place2", "Description", "Alice", 458, 156, Visibility.FRIENDS, Category.BAR),
-        new Place("", "Place3", "Description", "Alice", 456, 789, Visibility.FRIENDS, Category.RESTAURANT),
-        new Place("", "Place4", "Description", "Alice", 789, 456, Visibility.FRIENDS, Category.SHOP),
+        new Place("id1", "Place1", "Description", "Alice", 756, 123, Visibility.FRIENDS, Category.MONUMENT),
+        new Place("id2", "Place2", "Description", "Alice", 458, 156, Visibility.FRIENDS, Category.BAR),
+        new Place("id3", "Place3", "Description", "Alice", 456, 789, Visibility.FRIENDS, Category.RESTAURANT),
+        new Place("id4", "Place4", "Description", "Alice", 789, 456, Visibility.FRIENDS, Category.SHOP),
     ];
     let friend = new User("Alice", "", "");
     jest.spyOn(api, 'getPlacesToShareByUser').mockImplementation((): Promise<Place[]> => Promise.resolve(places))
+    jest.spyOn(api, 'getSharedPlacesByFriends').mockImplementation((): Promise<Place[]> => Promise.resolve(places))
     await act(async () => {
         
         const { container, getByText, getAllByText } = render(<FriendPanel friend={friend} friendPhoto={""} sharedSites={places}/>)
@@ -33,8 +34,10 @@ test('check comment is added', async () => {
         expect(getByText("SHOP")).toBeInTheDocument();
 
         let buttons = getAllByText("Show");
+        let button = buttons[0]
         await fireEvent.click(buttons[0]);
+        let buttons2 = getAllByText("Hide");
 
-        await waitFor(()=>expect(getByText("HIDE")).toBeInTheDocument());
+        await waitFor(()=>expect(getByText("Hide")).toBeInTheDocument());
     });
 })
