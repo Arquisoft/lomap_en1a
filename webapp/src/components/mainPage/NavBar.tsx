dbce48ae0a8ecc01bfab111fdc6c04c82296454b
UO281801 <UO281801@uniovi.es>
2023-05-02 17:28:24 +0200

    Update NavBar.tsx

--- webapp/src/components/mainPage/NavBar.tsx
+++ webapp/src/components/mainPage/NavBar.tsx
@@ -52,12 +52,7 @@ export default function NavBar(): JSX.Element {
 function LoggedNavbar(props: LogoutProps) {
   const getUrl = () => {
     let host = process.env.host || "localhost";
-    let url = window.location.href;
-    if (url.includes("https")) {
-      return "https://" + host + ":443/map"
-    } else {
-      return "http://" + host + ":80/map"
-    }
+    return "https://" + host + ":443/map"
 
   }
 
