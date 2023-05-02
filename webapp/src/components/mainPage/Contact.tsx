655e1e2825ab5bfe7f67a1bd0871fccf44e8cc8f
Diego-274 <diegoreyber@hotmail.com>
2023-05-02 20:06:17 +0200

    Test and fixes

--- webapp/src/components/mainPage/Contact.tsx
+++ webapp/src/components/mainPage/Contact.tsx
@@ -5,31 +5,30 @@ export default function Contact(){
     return (
         <>
           <Grid container className="centered-grid grid-text-area" spacing={3} justifyContent="space-around" 
-            width={'80%'} height={'80%'} marginLeft={'auto'} marginRight={'auto'} padding={"1em 1em"}>
+            width={'80%'} height={'80%'} marginLeft={'auto'} marginRight={'auto'} padding={"0em 1em"}>
             <Grid item xs={12} textAlign={"center"}>
               <Box
-                  marginLeft="auto"
-                  marginRight="auto"
-                  height="10%"
+                  sx={{ height: '15%' }}
                   display="flex"
                   justifyContent="center"
                   flexDirection="column">
                 <h1>Contact us!</h1>
               </Box>
               <Box
-                  marginLeft="auto"
-                  marginRight="auto"
-                  height="90%"
+                  sx={{ height: '65%' }}
                   display="flex"
                   justifyContent="center"
-                  flexDirection="column">
-                <p>
-                  We are more than happy to answer any question you may have about this app. If you have any, please, do not 
-                  hesitate to send a message to any of us! We are also willing to receive any kind of feedback, critic or suggestion 
-                  you may think about.
-                  <br/>
-                  These are our contact emails:
-                </p>
+                  flexDirection="column"
+                  paddingTop="1em"
+                  margin="0em">
+                <div>
+                  <p>
+                    We are more than happy to answer any question you may have about this app. If you have any, please, do not 
+                    hesitate to send a message to any of us! We are also willing to receive any kind of feedback, critic or suggestion 
+                    you may think about.
+                    <br/>
+                    These are our contact emails:
+                  </p>
                   <ul className="email">
                     <li>Vanesa Alonso Ramos: <a href="mailto:UO281801@uniovi.es">UO281801@uniovi.es</a></li>
                     <li>Álvaro González Erigoyen: <a href="mailto:UO282790@uniovi.es">UO282790@uniovi.es</a></li>
@@ -38,6 +37,7 @@ export default function Contact(){
                     <li>Sergio del Rey Álvarez: <a href="mailto:UO282497@uniovi.es">UO282497@uniovi.es</a></li>
                     <li>Pablo Valdés Fernández: <a href="mailto:UO282655@uniovi.es">UO282655@uniovi.es</a></li>
                   </ul>
+                </div>
               </Box>
             </Grid>
           </Grid>
